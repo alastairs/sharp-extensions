d055a270b129623f09a6fc3dc172607c4e549a4c
Alastair Smith <alastair.smith@red-gate.com>
2013-12-24 11:21:37 +0000

    Add new SharpExtensions project.
    First set of extensions: methods to create TimeSpans from integers in a
    more fluent way. Using these methods, you can now say 14.Days(), for
    example.

---
+++ SharpExtensions/IntegerDateTimeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SharpExtensions
+{
+    public static class IntegerDateTimeExtensions
+    {
+        public static TimeSpan Days(this int number)
+        {
+            return TimeSpan.FromDays(number);
+        }
+
+        public static TimeSpan Hours(this int number)
+        {
+            return TimeSpan.FromHours(number);
+        }
+
+        public static TimeSpan Milliseconds(this int number)
+        {
+            return TimeSpan.FromMilliseconds(number);
+        }
+
+        public static TimeSpan Minutes(this int number)
+        {
+            return TimeSpan.FromMinutes(number);
+        }
+
+        public static TimeSpan Seconds(this int number)
+        {
+            return TimeSpan.FromSeconds(number);
+        }
+
+        public static TimeSpan Ticks(this int number)
+        {
+            return TimeSpan.FromTicks(number);
+        }
+    }
+}
